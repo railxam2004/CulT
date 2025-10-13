4b7e2996014fcf5e7261d402ccdea9e3fff59ab8
railxam2004 <railxam70@gmail.com>
2025-10-13 10:37:03 +0300

    feat(users): password reset flow (reset/done/confirm/complete) with Yandex SMTP sender; RU templates and UX

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
-# users/urls.py
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
+from django.conf import settings
 
 app_name = 'users'
 
@@ -23,4 +23,39 @@ urlpatterns = [
 
     # заявка на статус организатора
     path('profile/organizer-request/', views.organizer_request, name='organizer_request'),
+
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="registration/password_reset_form.html",
+            email_template_name="registration/password_reset_email.txt",
+            html_email_template_name="registration/password_reset_email.html",
+            subject_template_name="registration/password_reset_subject.txt",
+            from_email=settings.DEFAULT_FROM_EMAIL,  # явное указание отправителя
+            success_url=reverse_lazy("users:password_reset_done"),
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="registration/password_reset_done.html",
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="registration/password_reset_confirm.html",
+            success_url=reverse_lazy("users:password_reset_complete"),
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="registration/password_reset_complete.html",
+        ),
+        name="password_reset_complete",
+    ),
 ]
